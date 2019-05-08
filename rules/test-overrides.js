b7597c993ee8f18d7f04c92623b40c0e40990663
ScottRudiger <scottrudiger@gmail.com>
2019-05-08 07:32:05 -0700

    Fix the way overrides is exported

--- rules/test-overrides.js
+++ rules/test-overrides.js
@@ -1,14 +1,12 @@
-module.exports = {
-  overrides: [{
-    files: ['*.test.js', '*.spec.js'],
-    rules: {
-      'prefer-rest-params': 'off',
-      'no-proto': 'off',
-      'require-jsdoc': 'off',
-      'func-names': 'off',
-      'no-await-in-loop': 'off',
-      'class-methods-use-this': 'off',
-      'no-sparse-arrays': 'off',
-    },
-  }],
-};
+module.exports = [{
+  files: ['*.test.js', '*.spec.js'],
+  rules: {
+    'prefer-rest-params': 'off',
+    'no-proto': 'off',
+    'require-jsdoc': 'off',
+    'func-names': 'off',
+    'no-await-in-loop': 'off',
+    'class-methods-use-this': 'off',
+    'no-sparse-arrays': 'off',
+  },
+}];
